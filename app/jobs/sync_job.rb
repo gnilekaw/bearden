705e5fdb2ff181fc2c357bd577e43863c50ad2ae
Lance W <lwakeling@gmail.com>
2017-03-29 09:05:40 -0400

    Use ENV to specify which Redshift database to write to

--- app/jobs/sync_job.rb
+++ app/jobs/sync_job.rb
@@ -2,8 +2,6 @@
 
 class SyncJob < ApplicationJob
   def perform
-    # TODO: Is there a place on staging that this can run?
-    # abort('Sync is only available on production') unless Rails.env.production?
     Sync.apply
   end
 end
